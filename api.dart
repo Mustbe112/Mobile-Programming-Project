4c2e4c8f5c13c3e6b8cb8c639ec348c8b69425b0
Willian <138235314+Mustbe112@users.noreply.github.com>
2025-11-20 19:35:53 +0700

    Update api.dart

--- api.dart
+++ api.dart
@@ -8,11 +8,29 @@ const String API_BASE = 'http://10.0.2.2:3000/api';
 class Api {
   static Future<http.Response> post(String path, Map body) {
     final url = Uri.parse('$API_BASE$path');
-    return http.post(url, headers: {'Content-Type': 'application/json'}, body: jsonEncode(body));
+    return http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(body),
+    );
   }
 
   static Future<http.Response> get(String path) {
     final url = Uri.parse('$API_BASE$path');
     return http.get(url);
+  }
+
+  static Future<http.Response> put(String path, Map body) {
+    final url = Uri.parse('$API_BASE$path');
+    return http.put(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(body),
+    );
+  }
+
+  static Future<http.Response> delete(String path) {
+    final url = Uri.parse('$API_BASE$path');
+    return http.delete(url);
   }
 }
